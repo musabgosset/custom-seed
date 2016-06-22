57857c9d8dff1163d7c041a8e75c00c0ac31b16d
Mathieu Renda <mathieu.renda@gmail.com>
2016-06-22 22:03:34 +0200

    Added test project and unified the frameworks

---
+++ test/CustomSeed.Web.Tests/FirstTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CustomSeed.Web.Tests
+{
+    public class FirstTest
+    {
+        public FirstTest()
+        {
+        }
+
+        [Fact]
+        public void PassMe()
+        {
+
+        }
+    }
+}
