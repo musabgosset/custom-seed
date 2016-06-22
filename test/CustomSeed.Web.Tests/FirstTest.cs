e9253ea143b2143256d00384afb52b82bf6bf1f5
Mathieu Renda <mathieu.renda@gmail.com>
2016-06-23 00:44:33 +0200

    Added HttpClient testing and net451 target

--- test/CustomSeed.Web.Tests/FirstTest.cs
+++ test/CustomSeed.Web.Tests/FirstTest.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace CustomSeed.Web.Tests
 {
-    public class FirstTest
+    public class FirstTest : IClassFixture<TestFixture<CustomSeed.Web.Startup>>
     {
-        public FirstTest()
+        private readonly TestFixture<CustomSeed.Web.Startup> _fixture;
+
+        public FirstTest(TestFixture<CustomSeed.Web.Startup> fixture)
         {
+            this._fixture = fixture;
         }
 
         [Fact]
-        public void PassMe()
+        public async Task HttpClientTest()
         {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/");
 
+            HttpResponseMessage response = await _fixture.Client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Contains("Hello World!", response.Content.ReadAsStringAsync().Result);
         }
     }
 }
