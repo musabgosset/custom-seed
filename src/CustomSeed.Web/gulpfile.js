8c2b5d35bf34ddaba06f060db082a174ba0caec4
Mathieu Renda <mathieu.renda@gmail.com>
2016-06-26 23:24:13 +0200

    Added TypesSript and typings along with 1st ng module

--- src/CustomSeed.Web/gulpfile.js
+++ src/CustomSeed.Web/gulpfile.js
@@ -1,6 +1,20 @@
 ﻿
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', function () {
-    gulp.src('node_modules/angular/angular.js').pipe(gulp.dest("wwwroot/libs"));
+gulp.task('default', ['scripts'], function () {
+    gulp.src('node_modules/angular/angular.js').pipe(gulp.dest('wwwroot/libs'));
+
+    var tsResult = tsProject.src().pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest('release'));
+
+});
+
+gulp.task('scripts', function () {
+
+    var tsResult = tsProject.src().pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest('wwwroot/app'));
 });
